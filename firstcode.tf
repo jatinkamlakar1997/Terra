3352f08e025ae71f984aaf07032eadf6247e1fbf
jatin <jatin>
2022-05-17 12:36:42 +0530

    --- Auto-Git Commit ---

--- firstcode.tf
+++ firstcode.tf
@@ -1,7 +1,8 @@
  provider "aws" {
      region = "us-east-1"
      access_key = "AKIAU3JYNQGTMXAKTTDL"
-     secret_key = ""
+     secret_key = "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"
+     
 
    
  }
