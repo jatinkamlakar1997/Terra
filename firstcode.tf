7ee7589b4c590b7b73c6a94c77d962cb7c3e3c23
jatin <jatin>
2022-05-17 12:37:12 +0530

    --- Auto-Git Commit ---

--- firstcode.tf
+++ firstcode.tf
@@ -3,6 +3,6 @@
      access_key = "AKIAU3JYNQGTMXAKTTDL"
      secret_key = "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"
  }
- resource "" "name" {
+ resource "aws_accessanalyzer_analyzer" "name" {
    
  }
