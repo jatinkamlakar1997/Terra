d1b82b4b9a5ee19fa6458b754c219fd0e3fbd5f9
jatin <jatin>
2022-05-17 12:37:52 +0530

    --- Auto-Git Commit ---

--- firstcode.tf
+++ firstcode.tf
@@ -3,6 +3,6 @@
      access_key = "AKIAU3JYNQGTMXAKTTDL"
      secret_key = "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"
  }
- resource "aws_instance" "ja" {
+ resource "aws_instance" "jattu" {
    
  }
